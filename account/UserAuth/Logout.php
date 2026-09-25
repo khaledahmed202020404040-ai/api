6780008979e97866fbce71e2aaad7bbf0a67794f
khaledahmed202020404040-ai <khaledahmed202020404040@gmail.com>
2026-09-25 18:36:20 +0300

    Remove logout response handling from Logout.php

--- account/UserAuth/Logout.php
+++ account/UserAuth/Logout.php
@@ -1,9 +1,2 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-
-echo json_encode([
-  'status' => 'success',
-  'message' => 'logged out',
-  'logout' => true
-], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-exit;
+require_once __DIR__ . '/../account/UserAuth/Logout.php';
